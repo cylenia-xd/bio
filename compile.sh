ab7dc01a9bf5ff7b00ca7b50a2ee180e63614601
cylenia <me@cylenia.dev>
2026-01-24 10:14:45 +1100

    fix again

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env nix-shell
 #! nix-shell -i bash --pure
 #! nix-shell -p bash nodejs
-#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/nixos-unstable
+#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz
 
 npx @11ty/eleventy
